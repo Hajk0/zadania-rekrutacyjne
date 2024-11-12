5f244ebc461a266782fcf465c3782734aca21383
Witold Hajkowicz <witold.hajkowicz@student.put.poznan.pl>
2024-11-12 19:07:12 +0100

    Done gites

--- python/main.py
+++ python/main.py
@@ -3,6 +3,34 @@
 # Wynik przedstaw w czytelnej formie na standardowym wyjściu
 
 import json
+import re
+
+def ctToOunce(ct: float) -> float:
+    return 0.00705479239 * ct
+
+def gToOunce(g: float) -> float:
+    return 0.352739619 * g
+
+def getMass(massString: str) -> float:
+    r = re.search(r'\d+[,\.]?\d*', massString)
+    if r:
+        strFloat = r.group().replace(',', '.')
+        return float(strFloat)
+    return None
+
+def calculateValue(row, converter):
+    mass = getMass(row['Masa'])
+    totalValue = converter(mass) * row['Wartość za uncję (USD)']
+    return totalValue
+
+def combineRow(rKat, rInput):
+    row = rInput.copy()
+    row['Wartość za uncję (USD)'] = int(rKat['Wartość za uncję (USD)'])
+    if row['Masa'][-1] == 'g':
+        row['Wartość'] = calculateValue(row, gToOunce)
+    else:
+        row['Wartość'] = calculateValue(row, ctToOunce)
+    return row
 
 kategories = None
 inputSet = None
@@ -14,15 +42,18 @@ sortedKategories = sorted(kategories, key=lambda x: -x["Wartość za uncję (USD
 with open('..\\dane\\zbiór_wejściowy.json', encoding='utf-8') as file:
     inputSet = json.load(file)
 
-header = f"{'Typ':<15}{'Czystość':<15}{'Wartość za uncję (USD)':<25}{'Masa':<15}{'Barwa':<15}{'Pochodzenie':<15}{'Właściciel':<25}"
-print(header)
-print(len(header) * '-')
-for rKat in sortedKategories:
-    first = True
+
+
+combinedSet = []
+for rKat in kategories:
     for rInput in inputSet:
         if rKat['Typ'] == rInput['Typ'] and rKat['Czystość'] == rInput['Czystość']:
-            if first == True:
-                print(f"{rKat['Typ']:<15}{rKat['Czystość']:<15}{rKat['Wartość za uncję (USD)']:<25}{rInput['Masa']:<15}{rInput['Barwa']:<15}{rInput['Pochodzenie']:<15}{rInput['Właściciel']:<15}")
-                first = False
-            else:
-                print(f"{'':<55}{rInput['Masa']:<15}{rInput['Barwa']:<15}{rInput['Pochodzenie']:<15}{rInput['Właściciel']:<15}")
+            row = combineRow(rKat, rInput)
+            combinedSet.append(row)
+
+sortedSet = sorted(combinedSet, key=lambda x: -x['Wartość'])
+header = f"{'Typ':<15}{'Czystość':<15}{'Wartość za uncję (USD)':<25}{'Masa':<15}{'Barwa':<15}{'Pochodzenie':<15}{'Właściciel':<50}{'Wartość':<10}"
+print(header)
+print(len(header) * '-')
+for row in sortedSet[:5]:
+    print(f"{row['Typ']:<15}{row['Czystość']:<15}{row['Wartość za uncję (USD)']:<25}{row['Masa']:<15}{row['Barwa']:<15}{row['Pochodzenie']:<15}{row['Właściciel']:<50}{row['Wartość']:<10.2f}")
